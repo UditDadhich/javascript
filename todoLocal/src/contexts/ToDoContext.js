a14d3e3511c2b3979a0d8510e73ad1950d69a853
UditDadhich <dadhichudit2@gmail.com>
2025-08-23 19:08:15 +0530

    Add files via upload

---
+++ todoLocal/src/contexts/ToDoContext.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { createContext , useContext } from "react";
+
+export const ToDoContext = createContext ({
+    todos : [
+        {
+            id : 1,
+            todo : "ToDo msg",
+            completed : false
+        }
+    ],
+    addToDo : (todo) => {},
+    updateToDo : (id , todo) => {},
+    deleteToDo : (id) => {},
+    toggleComplete : (id) => {}
+})
+
+export const useTodo = () => {
+    return useContext(ToDoContext);
+}
+
+export const ToDoProvider = ToDoContext.Provider;
